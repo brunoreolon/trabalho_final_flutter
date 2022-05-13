20a4da974dfddb114fd554e14abf78a34ff05bf5
Bruno Reolon <bruno.reolonn@gmail.com>
2022-05-13 06:23:51 -0300

    banco de dados

---
+++ lib/datasources/local/banco_dados.dart
@@ -0,0 +1,35 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+import 'local.dart';
+
+class BancoDados {
+  static const String _nomeBanco = 'trabalho_flutter.db';
+
+  static final BancoDados _intancia = BancoDados.internal();
+  factory BancoDados() => _intancia;
+  BancoDados.internal();
+
+  Database? _db;
+
+  Future<Database> get db async {
+    _db ??= await initDb();
+    return _db!;
+  }
+
+  Future<Database> initDb() async {
+    final path = await getDatabasesPath();
+    final pathDb = join(path, _nomeBanco);
+
+    return await openDatabase(pathDb, version: 1,
+      onCreate: (Database db, int version) async {
+            await db.execute(AlunoHelper.sqlCreate);
+      }
+    );
+  }
+
+  void close() async {
+    Database meuDb = await db;
+    meuDb.close();
+  }
+}
